530f5d418d88e0ee4ad6e6548467c3b1cb027be3
MKAbuMattar <mohammad.khaled@outlook.com>
2021-10-16 16:33:15 +0300

    hotfix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import router from './router/router'
 import { PORT, DATABASE_URL } from './config/variables.config'
-import onYourNetwork from './config/onYourNetwork.config'
 import connectDb from './config/db.config'
 
 connectDb(DATABASE_URL)
@@ -12,12 +11,4 @@ app.use(express.json())
 
 app.use(router)
 
-app.listen(PORT, () => {
-  // const API_URL = [`/api/v1/`]
-  // console.clear()
-  // console.info(`
-  //   Server running at
-  //     Local:            http://localhost:${PORT}/
-  //     On Your Network:  http://${Object.values(onYourNetwork)[0][0]}:${PORT}/
-  //     --------------`)
-})
+app.listen(PORT, () => {})
